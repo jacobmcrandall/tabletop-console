79270f097e4654e328a01fab14d784df9d228091
dev <@>
2026-01-30 07:48:16 -0500

    Paddle movement is rotary based instead of velocity based

--- pong/paddle/Paddle.cs
+++ pong/paddle/Paddle.cs
@@ -3,22 +3,10 @@ using System;
 
 public partial class Paddle : CharacterBody2D
 {
-	public const float Speed = 50.0f;
-
 	private float PreviousInputPosition {get; set;} = 0;
-
 
 	[Export]
 	public string RotaryAction = "0_rotary_moved";
-
-	public override void _PhysicsProcess(double delta)
-	{
-		MoveAndCollide(new Vector2(0, (float)(Velocity.Y * delta)));
-		Velocity = new Vector2(0, Velocity.Y - 1);
-		var screenSize = GetViewportRect().Size;
-		var paddleHeight = 32;
-		Position = new Vector2(Position.X, Math.Clamp(Position.Y, paddleHeight / 2, screenSize.Y - (paddleHeight / 2)));
-	}
 
     public override void _Input(InputEvent @event)
     {
@@ -26,8 +14,9 @@ public partial class Paddle : CharacterBody2D
 		{
 			var eRotaryAction = @event as ControllerInputEventAction;
 			var positionDiff = eRotaryAction.Position - PreviousInputPosition;
-			Velocity = new Vector2(0, positionDiff*Speed);
 			PreviousInputPosition = eRotaryAction.Position;
+			var moveDiff = positionDiff * 32;
+			Position = new Vector2(Position.X, Position.Y + moveDiff);
 
 		}
     }
